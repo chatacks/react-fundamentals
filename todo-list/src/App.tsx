adab4b9f686e99edb8ecfe7fb003868dddb01d75
Thyago Chatack <chatack.tc@gmail.com>
2026-04-01 09:55:44 -0300

    refactor: refactor App component to include header and global css

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,11 +1,11 @@
-
+import { Header } from './components/Header';
+import './global.css';
 function App() {
-
   return (
     <>
-    
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
