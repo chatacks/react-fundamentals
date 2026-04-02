76ac7f420041805e5616e9823f811efb242ae849
Thyago Chatack <chatack.tc@gmail.com>
2026-04-02 06:45:00 -0300

    feat: add Input component with styles for consistent design

---
+++ todo-list/src/components/Input.tsx
@@ -0,0 +1,16 @@
+import type { InputHTMLAttributes } from "react";
+import styles from './Input.module.css';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+} 
+
+export function Input({className, ...props}: InputProps) {
+  return (
+    <input
+      className={`${className} ${styles.input}`}
+      type="text"
+      {...props}
+    />
+  );
+}
