27e80d11ed73d5d0f75b17437788fe5c0feadb8a
Thyago Chatack <chatack.tc@gmail.com>
2026-04-03 14:30:10 -0300

    feat: add wrapper styles for App component layout

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,9 +1,14 @@
 import { Header } from './components/Header';
+import { NewTask } from './components/NewTask';
+import styles from './App.module.css';
 import './global.css';
 function App() {
   return (
     <>
       <Header />
+      <main className={styles.wrapper}>
+        <NewTask />
+      </main>
     </>
   );
 }
