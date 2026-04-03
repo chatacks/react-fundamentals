d98818927b128b847159a15db7bae8846a7bf46e
Thyago Chatack <chatack.tc@gmail.com>
2026-04-03 14:30:27 -0300

    feat: update Card component to accept props for task management and display

--- todo-list/src/components/Card.tsx
+++ todo-list/src/components/Card.tsx
@@ -2,21 +2,37 @@ import { TrashIcon } from '@phosphor-icons/react';
 import styles from './Card.module.css';
 import { Input } from './Input';
 import { Button } from './Button';
+import type { UUID } from './NewTask';
 
-export function Card() {
+interface CardProps {
+  id: UUID;
+  taskName: string;
+  isFinished: boolean;
+  onFinishedTask: (id: UUID, isFinishedTask: boolean) => void;
+  onDeleteTask: (id: UUID) => void;
+}
+
+export function Card({id, taskName, isFinished, onFinishedTask, onDeleteTask }: CardProps) {
   return (
     <div className={styles.wrapper}>
       <label className={styles.checkbox}>
         <Input
           className={styles.inputCard}
           type="checkbox"
+          id={id}
+          value={taskName}
+          checked={isFinished}
+          onChange={(event) => onFinishedTask(id, event.target.checked)}
         />
         <span className={styles.custom}></span>
       </label>
       
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus a quam eligendi doloremque incidunt ullam perspiciatis placeat illum blanditiis voluptatum.</p>
+      <p>{taskName}</p>
 
-      <Button className={styles.buttonCard}>
+      <Button
+        className={styles.buttonCard}
+        onClick={() => onDeleteTask(id)}
+      >
         <TrashIcon size={24}/>
       </Button>
     </div>
