a02b710e4a02ebe0209292b31e455728cd6be3a1
Thyago Chatack <chatack.tc@gmail.com>
2026-04-07 09:10:42 -0300

    feat: add DefaultLayout component with styled container and outlet

---
+++ pomodoro/src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: calc(100vh - 10rem);
+
+  margin: 5rem auto;
+  padding: 2.5rem;
+
+  background-color: var(--gray-600);
+  border-radius: .5rem;
+
+  display: flex;
+  flex-direction: column;
+`;
