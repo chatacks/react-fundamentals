6e56f8daa1ad79571192bd07368c5736e809be03
Thyago Chatack <chatack.tc@gmail.com>
2026-04-07 09:11:19 -0300

    feat: wrap App component with BrowserRouter for routing support

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+import { Router } from './Router';
 
 export function App () {
   return (
-    <div>
-
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
   );
 }
