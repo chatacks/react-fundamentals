99720f821eff2a681c72d2ab1087e37ef4a6eb59
Thyago Chatack <chatack.tc@gmail.com>
2026-04-09 09:35:00 -0300

    feat: wrap Router component with CyclesContextProvider for context management

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -3,12 +3,15 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
 import { Router } from './Router';
+import { CyclesContextProvider } from './contexts/CycleContext/CycleProvider';
 
 export function App () {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Router />
+      <CyclesContextProvider>
+        <Router />
+      </CyclesContextProvider>
     </ThemeProvider>
   );
 }
