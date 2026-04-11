41c6dd0f045dea7eeec746303b33cede33ebeef7
Thyago Chatack <chatack.tc@gmail.com>
2026-04-11 10:40:50 -0300

    feat: add global styles and default theme for styled-components integration

---
+++ coffee-delivery/src/styles/global.ts
@@ -0,0 +1,84 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    font: inherit;
+  }
+
+  :root {
+    --text-xs: ${({ theme }) => theme.fontSize.xs};
+    --text-sm: ${({ theme }) => theme.fontSize.sm};
+    --text-base: ${({ theme }) => theme.fontSize.base};
+    --text-lg: ${({ theme }) => theme.fontSize.lg};
+    --text-xl: ${({ theme }) => theme.fontSize.xl};
+    --text-2xl: ${({ theme }) => theme.fontSize.xl2};
+    --text-3xl: ${({ theme }) => theme.fontSize.xl3};
+    --text-5xl: ${({ theme }) => theme.fontSize.xl5};
+
+    --fw-regular: ${({ theme }) => theme.fontWeight.regular};
+    --fw-bold: ${({ theme }) => theme.fontWeight.bold};
+
+    --ff-roboto: ${({ theme }) => theme.fontFamily.roboto};
+    --ff-baloo: ${({ theme }) => theme.fontFamily.baloo};
+
+    --white: ${({ theme }) => theme.colors.white};
+
+    --gray-100: ${({ theme }) => theme.colors.gray100};
+    --gray-200: ${({ theme }) => theme.colors.gray200};
+    --gray-300: ${({ theme }) => theme.colors.gray300};
+    --gray-400: ${({ theme }) => theme.colors.gray400};
+    --gray-500: ${({ theme }) => theme.colors.gray500};
+    --gray-600: ${({ theme }) => theme.colors.gray600};
+    --gray-700: ${({ theme }) => theme.colors.gray700};
+    --gray-800: ${({ theme }) => theme.colors.gray800};
+    --gray-900: ${({ theme }) => theme.colors.gray900};
+
+    --yellow-100: ${({ theme }) => theme.colors.yellow100};
+    --yellow-500: ${({ theme }) => theme.colors.yellow500};
+    --yellow-700: ${({ theme }) => theme.colors.yellow700};
+
+    --purple-100: ${({ theme }) => theme.colors.purple100};
+    --purple-500: ${({ theme }) => theme.colors.purple500};
+    --purple-700: ${({ theme }) => theme.colors.purple700};
+  }
+
+  html {
+    font-family: var(--ff-roboto);
+    font-size: var(--text-base);
+    font-weight: var(--fw-regular);
+    color: var(--gray-700);
+    line-height: 1.3;
+
+    scroll-behavior: smooth;
+  }
+
+  body {
+    background-color: var(--gray-100);
+  }
+
+  button,
+  input,
+  optgroup,
+  select,
+  text-area,
+  a {
+    color: inherit;
+    font-family: inherit;
+    font-size: inherit;
+    font-weight: inherit;
+    line-height: inherit;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+`;
---
+++ coffee-delivery/src/styles/themes/default.ts
@@ -0,0 +1,45 @@
+export const defaultTheme = {
+  fontFamily: {
+    roboto: '"Roboto", system-ui, sans-serif',
+    baloo: '"Baloo 2", sans-serif',
+  },
+
+  fontWeight: {
+    regular: 400,
+    bold: 700,
+  },
+
+  fontSize: {
+    xs: '0.75rem',
+    sm: '0.875rem',
+    base: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    xl2: '1.5rem',
+    xl3: '2rem',
+    xl5: '3rem',
+  },
+
+  colors: {
+    white: '#ffffff',
+
+    gray100: '#fafafa',
+    gray200: '#f3f2f2',
+    gray300: '#ededed',
+    gray400: '#e6e5e5',
+    gray500: '#d7d5d5',
+    gray600: '#8d8686',
+    gray700: '#574f4d',
+    gray800: '#403937',
+    gray900: '#272221',
+
+    yellow100: '#f1e9c9',
+    yellow500: '#dbac2c',
+    yellow700: '#c47f17',
+
+    purple100: '#ebe5f9',
+    purple500: '#8047f8',
+    purple700: '#4b2995',
+  }
+
+};
