1815f0ef6887af1cbd7d6c6660e0172ba395dc4c
Thyago Chatack <chatack.tc@gmail.com>
2026-04-22 09:06:41 -0300

    feat: enhance CoffeesProvider and context to manage new order state and pricing calculations

--- coffee-delivery/src/context/CoffeesProvider.tsx
+++ coffee-delivery/src/context/CoffeesProvider.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { CoffeesContext } from '.';
 import type { CoffeeType } from '../@types/coffees.types';
+import { parsePrice } from '../pages/Checkout/utils/parsePrice';
+import type { NewOrderFormData } from '../pages/Checkout/components/CheckoutForm';
 
 interface CoffeesProviderProps {
   children: React.ReactNode;
@@ -10,8 +12,17 @@ export type CoffeeToOrder = Omit<CoffeeType, 'badges' | 'description'> & {
   quantity: number;
 }
 
+export type NewOrder = Omit<NewOrderFormData, 'cep' | 'additional' | 'neighborhood'>
+
 export function CoffeesProvider({ children }: CoffeesProviderProps) {
   const [orderedsCoffee, setOrderedsCoffee] = useState<CoffeeToOrder[]>([]);
+  const [order, setOrder] = useState<NewOrder>({
+    city: '',
+    number: '',
+    payment: 'credito',
+    state: '',
+    street: ''
+  });
 
   const handleAddCoffeeToOrder = (coffee: CoffeeToOrder) => {
     setOrderedsCoffee(prevState => {
@@ -25,11 +36,26 @@ export function CoffeesProvider({ children }: CoffeesProviderProps) {
     });
   };
 
+  const newOrder = (data: NewOrder) => {
+    setOrder(data);
+  };
+
+  const totalPriceItems = orderedsCoffee
+    .reduce((acc, curr) => acc + parsePrice(curr.price) * curr.quantity, 0);
+  const deliveryPrice = 3.5;
+  const totalPrice = totalPriceItems + deliveryPrice;
+
+
   return (
     <CoffeesContext
       value={{
         orderedsCoffee,
-        handleAddCoffeeToOrder
+        handleAddCoffeeToOrder,
+        totalPriceItems,
+        deliveryPrice,
+        totalPrice,
+        newOrder,
+        order
       }}
     >
       {children}
--- coffee-delivery/src/context/index.ts
+++ coffee-delivery/src/context/index.ts
@@ -1,9 +1,14 @@
 import { createContext } from 'react';
-import type { CoffeeToOrder } from './CoffeesProvider';
+import type { CoffeeToOrder, NewOrder } from './CoffeesProvider';
 
 interface CoffeesContextType {
   orderedsCoffee: CoffeeToOrder[];
   handleAddCoffeeToOrder: (coffee: CoffeeToOrder) => void;
+  totalPriceItems: number;
+  deliveryPrice: number;
+  totalPrice: number;
+  newOrder: (data: NewOrder) => void;
+  order: NewOrder,
 }
 
 
