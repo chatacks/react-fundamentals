f4e571efca710e1a45e80809f29a7636b7ba1924
Thyago Chatack <chatack.tc@gmail.com>
2026-05-01 10:38:29 -0300

    feat: implement routing and theming in the application

--- github-blog/src/App.tsx
+++ github-blog/src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/theme/default';
+import { GlobalStyle } from './styles/global';
+import { Router } from './Router';
+
 export function App() {
 
   return (
-    <>
-    </>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+      <Router />
+    </ThemeProvider>
+  );
 }
 
