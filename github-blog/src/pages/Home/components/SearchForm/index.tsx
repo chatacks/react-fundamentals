45573941ac1b9ecfd4ef34a2224d45f23d0bae6b
Thyago Chatack <chatack.tc@gmail.com>
2026-05-07 07:40:16 -0300

    feat: update SearchForm to handle query submission and display item count

--- github-blog/src/pages/Home/components/SearchForm/index.tsx
+++ github-blog/src/pages/Home/components/SearchForm/index.tsx
@@ -1,25 +1,35 @@
 import { useState } from 'react';
 import { Input, SearchFormContainer } from './styles';
+import { useFetchIssue } from '../../../../hooks/useFetchIssue';
 
-export function SearchForm() {
+export function SearchForm({ items } : { items: number }) {
   const [query, setQuery] = useState('');
+  const { fetchIssues } = useFetchIssue();
 
-  const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value);
+  const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setQuery(e.target.value);
+
+  const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>, query: string) => {
+    e.preventDefault();
+
+    fetchIssues(query);
+    setQuery('');
+  };
 
   return (
-    <SearchFormContainer>
+    <SearchFormContainer onSubmit={(e) => handleSubmit(e, query)}>
       <div>
         <span>Publicações</span>
-        <span>6 publicações</span>
+        <span>{items > 1 ? `${items} publicações` : `${items} publicação`}</span>
       </div>
 
       <Input
-        type="text"
-        name="query"
         value={query}
+        type="text"
         placeholder="Buscar conteúdo"
         onChange={handleQueryChange}
       />
+
     </SearchFormContainer>
   );
 };
--- github-blog/src/pages/Home/components/SearchForm/styles.ts
+++ github-blog/src/pages/Home/components/SearchForm/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const SearchFormContainer = styled.div`
+export const SearchFormContainer = styled.form`
   display: flex;
   flex-direction: column;
 
